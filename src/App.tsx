8bdb8470e6503d2803c1fa44d8fbe9c63cab032c
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-12-24 11:06:15 +0330

    chore: floara finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import FroalaEditor from 'react-froala-wysiwyg';
-// Require Editor CSS files.
 import 'froala-editor/css/froala_style.min.css';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'froala-editor/js/plugins.pkgd.min.js';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/js/languages/fa.js';
 import './App.css';
 
 export default function App() {
   return (
-    <div id=''>
+    <div className='container'>
       <div>text editor </div>
-      <div id='editor'>
+      <div className='editor'>
         <FroalaEditor
           config={{
             placeholderText: 'بنویسید...',
@@ -16,12 +19,61 @@ export default function App() {
             charCounterCount: true,
             toolbarButtons: {
               moreText: {
-                buttons: ['bold', 'italic', 'underline', 'strikethrough'],
+                buttons: [
+                  'bold',
+                  'italic',
+                  'underline',
+                  'strikethrough',
+                  'subscript',
+                  'superscript',
+                  'fontFamily',
+                  'fontSize',
+                  'textColor',
+                  'backgroundColor',
+                  'inlineClass',
+                  'inlineStyle',
+                  'clearFormatting',
+                ],
               },
               moreParagraph: {
-                buttons: ['alignLeft', 'alignRight', 'alignCenter'],
+                buttons: [
+                  'alignLeft',
+                  'alignCenter',
+                  'alignRight',
+                  'formatOL',
+                  'formatUL',
+                  'outdent',
+                  'indent',
+                  'quote',
+                  'paragraphFormat',
+                  'paragraphStyle',
+                  'lineHeight',
+                  'alignJustify',
+                ],
               },
-              moreRich: { buttons: ['insertLink', 'insertHR', 'undo', 'redo'] },
+              moreRich: {
+                buttons: [
+                  'insertLink',
+                  'insertImage',
+                  'insertVideo',
+                  'insertTable',
+                  'emoticons',
+                  'fontAwesome',
+                  'specialCharacters',
+                  'embedly',
+                  'insertFile',
+                  'insertHR',
+                ],
+              },
+              paragraphFormat: {
+                N: 'Normal',
+                H1: 'Heading 1',
+                H2: 'Heading 2',
+                H3: 'Heading 3',
+                H4: 'Heading 4',
+                buttonsVisible: 2,
+              },
+              paragraphFormatSelection: true,
             },
           }}
         />
